92949593fdf044b72af8f6c6c0286c738342433d
babli-012 <babligour3467@gmail.com>
2025-01-06 15:49:33 +0530

    remove unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-
 import './App.css'
-import Toggleswitch from './toggleswitch/toggleswitch'
 import Todo_list from './todo_list/todo_list'
-import Test_counter from './Test_counter'
-
-
-
 function App() {
   return (
     <>
-      {/* <Toggleswitch/> */}
       <Todo_list/>
-      {/* <Test_counter/> */}
-      
     </>
   )
 }
